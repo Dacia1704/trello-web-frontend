129deadd87df4ba77e3ead88ca35cd7cffa2567e
Seichi04 <duongbacdinhthoa@gmail.com>
2025-07-06 01:05:28 +0700

    fix smt

--- eslint.config.js
+++ eslint.config.js
@@ -2,26 +2,49 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
 
 export default defineConfig([
-    {
-        files: ["**/*.{js,mjs,cjs,jsx}"],
-        plugins: { js },
-        extends: ["js/recommended"],
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js, react: pluginReact, "react-hooks": pluginReactHooks, "react-refresh": pluginReactRefresh },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
     },
-    {
-        files: ["**/*.{js,mjs,cjs,jsx}"],
-        languageOptions: {
-            globals: globals.browser,
-        },
-    },
-    pluginReact.configs.flat.recommended,
+  },
+  pluginReact.configs.flat.recommended,
 
-    // 👇 Thêm đoạn này để tắt rule 'react/react-in-jsx-scope'
-    {
-        files: ["**/*.{js,jsx,ts,tsx}"],
-        rules: {
-            "react/react-in-jsx-scope": "off",
-        },
+  // 👇 Thêm đoạn này để tắt rule 'react/react-in-jsx-scope'
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react-refresh/only-export-components": "warn",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/prop-types": 0,
+      "react/display-name": 0,
+
+      "no-console": 1,
+      "no-lonely-if": 1,
+      "no-unused-vars": 1,
+      "no-trailing-spaces": 1,
+      "no-multi-spaces": 1,
+      "no-multiple-empty-lines": 1,
+      "space-before-blocks": ["error", "always"],
+      "object-curly-spacing": [1, "always"],
+      indent: ["warn", 2],
+      "array-bracket-spacing": 1,
+      "linebreak-style": 0,
+      "no-unexpected-multiline": "warn",
+      "keyword-spacing": 1,
+      "comma-spacing": 1,
+      "arrow-spacing": 1,
     },
+  },
 ]);
--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { AccessAlarm } from "@mui/icons-material";
+import { ThreeDRotation } from "@mui/icons-material";
+import { HomeRepairService } from "@mui/icons-material";
 
 function App() {
-    const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>Dacia</div>
 
-    return (
-        <>
-            <div>
-                <a href="https://vite.dev" rel="noreferrer" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" rel="noreferrer" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
-    );
+      <Typography variant="body2" color="text.secondary">
+        123
+      </Typography>
+
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+
+      <br />
+      <AccessAlarm />
+      <ThreeDRotation />
+      <HomeRepairService color="primary" />
+    </>
+  );
 }
 
 export default App;
