0ec135038551885e5fa588324fe5a388d23a7b8c
Seichi04 <duongbacdinhthoa@gmail.com>
2025-07-05 18:28:07 +0700

    config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,27 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        plugins: { js },
+        extends: ["js/recommended"],
     },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        languageOptions: {
+            globals: globals.browser,
+        },
     },
-  },
-])
+    pluginReact.configs.flat.recommended,
+
+    // 👇 Thêm đoạn này để tắt rule 'react/react-in-jsx-scope'
+    {
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        rules: {
+            "react/react-in-jsx-scope": "off",
+        },
+    },
+]);
