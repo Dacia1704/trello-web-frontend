4226611084f03554dd2f2212afdc32fb67314c24
Seichi04 <duongbacdinhthoa@gmail.com>
2025-07-11 14:18:07 +0700

    add

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,18 +1,27 @@
 import Box from "@mui/material/Box";
 import ModeSelect from "~/components/ModeSelect";
+import AppsIcon from "@mui/icons-material/Apps";
+import { ReactComponent as trelloLogo } from "~/assets/trello.svg";
+import SvgIcon from "@mui/icons-material/Apps";
 
 export default function AppBar() {
   return (
     <Box
       sx={{
-        backgroundColor: "primary.light",
         width: "100%",
         height: (theme) => theme.trello.appBarHeight,
         display: "flex",
         alignItems: "center",
+        justifyContent: "space-between",
       }}
     >
-      <ModeSelect />
+      <Box>
+        <AppsIcon sx={{ color: "primary.main" }} />
+        <SvgIcon component={trelloLogo} inheritViewBox />
+      </Box>
+      <Box>
+        <ModeSelect />
+      </Box>
     </Box>
   );
 }
